57d3f80bf5c6969ad24b94058592264511426055
Aung Kyaw Khaing <76258097+iPanda007@users.noreply.github.com>
2024-05-24 00:58:05 +0630

    feat: test env

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 
+import { useEffect } from 'react'
 import AdminRoute from './routes/AdminRoutes'
 import { APP_API, Mode } from './services/environment'
 
 
 const App = () => {
 
-
+  useEffect(()=>{
+    fetch(APP_API)
+    .then(response => response.json())
+    .then(_json => {
+       alert('Fetch success')
+    })
+  },[])
   return (
     <div>
    continuous Integration code lint and type lint Aung Kyaw Khaing {Mode}  {APP_API}
