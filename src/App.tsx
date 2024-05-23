e697dc383b0cabe51e89f0fef081e891bb1b0f9c
Aung Kyaw Khaing <76258097+iPanda007@users.noreply.github.com>
2024-05-23 19:31:39 +0630

    fix: mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import AdminRoute from './routes/AdminRoutes'
-import { Mode } from './services/environment'
+
 
 const App = () => {
 
- console.log("afsdf")
- const x = 1;
+
   return (
     <div>
-   continuous Integration code lint and type lint {Mode}
+   continuous Integration code lint and type lint 
         <AdminRoute/>
     </div>
   )
